3de0aef6edc08dc6ad8ac4d969db1bcf1767f5b6
arun kumar <kashyaparun828@gmail.com>
2022-10-26 15:03:42 +0530

    add message in sum.java

--- sum.java
+++ sum.java
@@ -2,5 +2,5 @@ class sum{
     public static void main(String[] args){
         int a=2,b=3,c;
         c=a+b;
-        System.out .println("sum is " +c);
+        System.out .println("sum of a and b is " +c);
     }}
