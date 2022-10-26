8e5a9841a2f661b84453c71b7b9cb8e77305dc02
arun kumar <kashyaparun828@gmail.com>
2022-10-26 16:45:20 +0530

    modify result reresentation

--- diff.java
+++ diff.java
@@ -2,5 +2,5 @@ class diff{
     public static void main(String[] args){
         int a=2,b=3,c;
         c=a-b;
-        System.out .println("diff of a and b is " +c);
+        System.out .println("result is " +c);
     }}
