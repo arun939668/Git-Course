cb0f19966cd94cbd85addef5bda4e1454bac440b
arun kumar <kashyaparun828@gmail.com>
2022-11-02 08:46:50 +0530

    Using user define inputs

--- multiply.java
+++ multiply.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 class multiply{
     public static void main(String[] args){
-        int a=2,b=3,c;
+        int a,b,c;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the the value of a:");
+        a = input.nextInt();
+        System.out.println("Enter the the value of b:");
+        b = input.nextInt();
         c=a*b;
-        System.out .println("multiply of a and b is " +c);
+        System.out .println("Result is " +c);
+        input.close();
     }}
